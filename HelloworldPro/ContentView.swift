71c2baae0525a3f1ce2a93b5e6bd647059c5addd
sharuban <s.s.ruban98@gmail.com>
2025-04-24 18:13:09 +0530

    some coding changes

--- HelloworldPro/ContentView.swift
+++ HelloworldPro/ContentView.swift
@@ -14,13 +14,31 @@ struct ContentView: View {
             startPoint: .leading,
             endPoint: .trailing
         )
+    
+        let color = LinearGradient (
+            colors: [.red, .orange, .yellow, .green, .blue, .purple],
+            startPoint:.topLeading ,
+            endPoint: .bottomTrailing
+        )
         
+        let print = LinearGradient (
+            colors: [.black, .gray],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+    
     var body: some View {
-        Text(" Hello, \(Text("world").foregroundStyle(gradient)) ! ")
-            .bold()
-            .font(.title)
-            .textCase(.uppercase)
-        
+            Text(" Hello, \(Text("world").foregroundStyle(gradient)) ! ")
+                .bold()
+                .font(.title)
+                .textCase(.uppercase)
+        HStack {
+            Text ("iOS \(Text("Developer").foregroundStyle(color))")
+                .bold()
+                .font(.title2)
+            Image(systemName:"apple.logo").foregroundStyle(print)
+                .imageScale(.large)
+        }
     }
     
 }
